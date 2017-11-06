77bcedf6d9c8b4094a7a9d93716fdc2ba99ab1ab
Ernev Sharma <ernev.sharma.us@gmail.com>
2017-11-06 01:15:59 -0500

    Add files via upload

---
+++ Worker.java
@@ -0,0 +1,20 @@
+public class Worker extends Human
+{
+	
+	private String job;
+	private int children;
+	
+	public Worker(String fn, int e, int a, String j, int c) {
+		super(fn, e, a);
+		job = j;
+		children = c;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
